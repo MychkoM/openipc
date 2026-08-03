daf934c574a26aaeb54177184ee4423c1b41679f
Maxim Mycko <maxim.mycko@alza.cz>
2026-08-03 22:38:27 +0200

    Added Perenio PEIFC01 cam fix

--- general/package/hisilicon-osdrv-hi3516cv200/hisilicon-osdrv-hi3516cv200.mk
+++ general/package/hisilicon-osdrv-hi3516cv200/hisilicon-osdrv-hi3516cv200.mk
@@ -69,6 +69,7 @@ define HISILICON_OSDRV_HI3516CV200_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/sensors $(HISILICON_OSDRV_HI3516CV200_PKGDIR)/files/sensor/libsns_ar0237.so
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/sensors $(HISILICON_OSDRV_HI3516CV200_PKGDIR)/files/sensor/libsns_gc1034.so
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/sensors $(HISILICON_OSDRV_HI3516CV200_PKGDIR)/files/sensor/libsns_gc2023.so
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/sensors $(HISILICON_OSDRV_HI3516CV200_PKGDIR)/files/sensor/libsns_gc2033.so
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/sensors $(HISILICON_OSDRV_HI3516CV200_PKGDIR)/files/sensor/libsns_imx122.so
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/sensors $(HISILICON_OSDRV_HI3516CV200_PKGDIR)/files/sensor/libsns_imx291.so
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/sensors $(HISILICON_OSDRV_HI3516CV200_PKGDIR)/files/sensor/libsns_imx323_i2c_dc_v1.so
